a74a41cbff3a73ceb323dd615d8822a8bd61ebc2
belka <izaa1109@gmail.com>
2020-03-02 18:00:35 +0100

    lab 0, Tails Generator

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,16 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        var tailList = new ArrayList<String>(value.length());
+        String temp = value;
+        for(int i = 0; i < value.length(); ++i){
+            tailList.add(temp);
+            temp = temp.substring(0, temp.length() - 1);
+        }
+        tailList.add("");
+
+        return tailList;
+        //return new ArrayList<>(value.length());
     }
 
 }
